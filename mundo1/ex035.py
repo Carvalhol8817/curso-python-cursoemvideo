0c65f9c7474a72d8235a12149e9c00eef2de33ea
Marcelo Carvalho Batista <Marcelocarvalho8817@gmail.com>
2026-05-14 03:11:41 -0300

    Correção ex035

--- mundo1/ex035.py
+++ mundo1/ex035.py
@@ -3,7 +3,7 @@ n1=float(input(f'{'\033[7;40m'}digite o primeiro comprimento:'))
 n2=float(input('digite o segundo comprimento:'))
 n3=float(input('digite o terceiro comprimento:'))
 
-if n1<n2+n3 and n3<n1+n3 and n3<n2+n1:
+if n1<n2+n3 and n2<n1+n3 and n3<n2+n1:
     print(f'{'\033[0;32m'}Esses comprimentos podem formar um triangulo{'\033[m'}')
 else:
     print(f'{'\033[0;31m'}Esses comprimentos nao podem formar um triangulo{'\033[m'}')
